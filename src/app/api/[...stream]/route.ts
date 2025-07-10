2ccadd3640da1f5ac75bf625ab5fcab238814384
iDaedalus <jerometecsonn@gmail.com>
2025-07-10 21:16:51 +0800

    123

--- src/app/api/[...stream]/route.ts
+++ src/app/api/[...stream]/route.ts
@@ -216,7 +216,7 @@ export async function GET(
     "https://zxcstream.pro",
     "https://www.zxcstream.pro",
     "https://zxcstream-api.vercel.app",
-    "https://zxcstream-api-six.vercel.app",
+   
   ];
   
 
